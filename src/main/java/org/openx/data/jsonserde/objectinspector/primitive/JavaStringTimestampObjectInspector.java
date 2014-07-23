0fa39c53f74334b8dcf60e028f36966d3e44a4d7
Brock Noland <brock@cloudera.com>
2014-07-23 13:31:47 -0400

    Update additional object inspectors

--- src/main/java/org/openx/data/jsonserde/objectinspector/primitive/JavaStringTimestampObjectInspector.java
+++ src/main/java/org/openx/data/jsonserde/objectinspector/primitive/JavaStringTimestampObjectInspector.java
@@ -15,7 +15,7 @@ package org.openx.data.jsonserde.objectinspector.primitive;
 import java.sql.Timestamp;
 import org.apache.hadoop.hive.serde2.io.TimestampWritable;
 import org.apache.hadoop.hive.serde2.objectinspector.primitive.AbstractPrimitiveJavaObjectInspector;
-import org.apache.hadoop.hive.serde2.objectinspector.primitive.PrimitiveObjectInspectorUtils;
+import org.apache.hadoop.hive.serde2.typeinfo.TypeInfoFactory;
 import org.apache.hadoop.hive.serde2.objectinspector.primitive.SettableTimestampObjectInspector;
 
 /**
@@ -26,7 +26,7 @@ public class JavaStringTimestampObjectInspector extends AbstractPrimitiveJavaObj
     implements SettableTimestampObjectInspector {
     
     public JavaStringTimestampObjectInspector() {
-        super(PrimitiveObjectInspectorUtils.timestampTypeEntry);
+        super(TypeInfoFactory.timestampTypeInfo);
     }
 
     
